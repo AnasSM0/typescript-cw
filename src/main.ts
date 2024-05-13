6afef97b2aa0ac4279cd80d20655983b23de642f
AnasSM0 <anasbutt20067@gmail.com>
2024-05-13 10:22:52 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+// function myInfo():void {
+
+// console.log("Its a me mario.");
+
+// }
+
+// myInfo();
+
+
+function myInfo(name:string, age?:number){
+
+console.log(`my name is ${name}`);
+console.log(`I am ${age} years old`);
+}
+
+myInfo("anas")
